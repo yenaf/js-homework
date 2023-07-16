dc1cd06f3785e230fc5051596709751dbcc554ca
yena <spffhd0835@gmail.com>
2023-07-16 23:53:10 +0900

    함수로 쪼개고 , 상태변수 추가

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -20,45 +20,71 @@ function emailReg(text){
 }
 
 function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{8,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+function $(node){
+  if(typeof node === 'string'){
+    node = document.querySelector(node);
+  }
 
-const id = document.querySelector('.user-email-input');
-const pw = document.querySelector('.user-password-input');
-const button = document.querySelector('.btn-login');
+  return node;
+}
 
-// let emailPass = false;
-id.addEventListener('input',function(){
-  if(emailReg(this.value)){
-    this.classList.remove('is--invalid')
-    // emailPass = true;
+const id = $('.user-email-input');
+const pw = $('.user-password-input');
+const button = $('.btn-login');
+
+//상태변수
+let checkUser = false;
+
+//아이디 체크 함수
+function handleEmailCheck(){
+  if(emailReg(this.value) || this.value === ''){
+    this.classList.remove('is--invalid');
+    checkUser = true;
   }else{
     this.classList.add('is--invalid');
-    // emailPass = false;
+    checkUser = false;
   }
-})
+}
 
-pw.addEventListener('input',function(){
-  if(pwReg(this.value)){
-    this.classList.remove('is--invalid')
-    // emailPass = true;
+//비밀번호 체크 함수
+function handlePwCheck(){
+  if(pwReg(this.value) || this.value === ''){
+    this.classList.remove('is--invalid');
+    checkUser = true;
   }else{
     this.classList.add('is--invalid');
-    // emailPass = false;
+    checkUser = false;
   }
-})
+}
 
-button.addEventListener('click',function(){
-  if(id.value === user.id || pw.value === user.pw){
-    window.location.href = 'welcome.html'
-  }else{
-    alert(' 아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.입력하신 내용을 다시 확인해주세요.')
+//버튼 클릭 체크 함수
+function handleBtnCheck(e){
+  e.preventDefault();
+  if(checkUser){
+    if(id.value === user.id && pw.value === user.pw){
+      window.location.href = 'welcome.html'
+    }else if(id.value === ''){
+      alert('아이디를 입력해주세요.');
+    }else if(pw.value === ''){
+      alert('비밀번호를 입력해주세요.');
+    }
+    else{
+      alert(' 아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.입력하신 내용을 다시 확인해주세요.')
+    }
   }
-})
+
+}
 
 
+
+
+id.addEventListener('input',handleEmailCheck);
+pw.addEventListener('input',handlePwCheck)
+button.addEventListener('click',handleBtnCheck);
 
 
 
